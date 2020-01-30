379cb18182b77ab0c9bde64068218c63f8d2894e
richard dinh <richardndinh@gmail.com>
2020-01-30 05:52:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,10 @@ def reverse_each_word(string)
 end
 
 def reverse_each_word_with_each(string)
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do |string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
